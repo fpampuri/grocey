c32624c479af8296feba227339ceb07417fb6902
pabloGoros <pgorostiaga@itba.edu.ar>
2025-10-12 11:03:25 -0300

    fix: Redirect to login always

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -7,7 +7,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  { path: '/', redirect: '/lists' },
+  { path: '/', redirect: '/login' },
   {
     path: '/login',
     name: 'login',
